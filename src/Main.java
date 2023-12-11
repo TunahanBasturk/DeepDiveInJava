4009b08f089e5458992b0535475ad1e33a88a038
Tunahan Basturk <tunahan.basturk1907@gmail.com>
2023-12-11 23:28:39 +0300

    ikinci ödev

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import java.awt.*;
+import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
-        int a = 10, b = 2, UcgenTaban = 15, ucgenYukseklik = 12, isaret = -3,
+ /*       int a = 10, b = 2, UcgenTaban = 15, ucgenYukseklik = 12, isaret = -3,
                 faizOrani = 25, sure = 300, not = 75, taban = 3, us = 3;
         long sonucUs =1;
         double anaPara = 35500.0;
@@ -64,7 +65,85 @@ public class Main {
             System.out.println("En Büyük Sayi us");
         }
 
+*/
+        //  ------------------          İKİNCİ ÖDEV        ------------------
+//         Birinci soru
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Cümleyi Giriniz: ");
+        String cumle = scanner.nextLine();
+        int boslukSayisi = 0;
+        for (int i=0;i<cumle.length();i++){
+            if(cumle.charAt(i) ==' ' ){
+                boslukSayisi++;
+            }
 
+        }
+
+        System.out.println("Cümledeki boşluk sayısı: "+ boslukSayisi);
+
+        //İkinci Soru
+
+        System.out.print("Kelime giriniz: ");
+        int c = 0;
+        String kelime = scanner.nextLine();
+        for (int j = 1; j <= kelime.length(); j++) {
+            if (!(kelime.charAt(c) == kelime.charAt(kelime.length() - j))) {
+                System.out.println("Girilen Kelime Palindrom Değildir");
+                break;
+            } else {
+                if(j==kelime.length()){
+                    System.out.println("Girilen Kelime Palindromdur.");
+                }
+            }
+            c++;
+        }
+/// Üçüncü Soru ---------OLMADI
+
+     /*   String tekrarEdenKelime = "Java";
+
+        for(int k=0; k<=cumle.length(); k++ ){
+            for (int l=0; l<=tekrarEdenKelime.length(); l++){
+            if (cumle.charAt(k)==tekrarEdenKelime.charAt(l)) {
+
+            }
+
+            }
+        }*/
+
+        //  10. Soru
+        int sesliHarf = 0;
+        int sessizHarf = 0;
+        for (int k=0; k<kelime.length();k++) {
+            if (kelime.charAt(k)=='a' || kelime.charAt(k)=='e' || kelime.charAt(k)=='ı' || kelime.charAt(k)=='i'
+                    || kelime.charAt(k)=='o' || kelime.charAt(k)=='ö' || kelime.charAt(k)=='e'
+                    || kelime.charAt(k)=='u' || kelime.charAt(k)=='ü'){
+                sesliHarf++;
+
+            }
+            else {
+                sessizHarf++;
+            }
+        }
+        System.out.println("Sesli Harf Sayısı: " + sesliHarf );
+        System.out.println("Sessiz Harf Sayısı: " + sessizHarf );
+
+        //   4. Soru
+
+        for (int z=1; z<= cumle.length();z++){
+            System.out.print(cumle.charAt(cumle.length()-z));
+        }
+
+        //          7. Soru
+
+        for (int x=0; x<cumle.length();x++){
+            if(cumle.charAt(x)==' '){
+              //  System.out.print(cumle.toUpperCase(x+1));
+                System.out.print(cumle.charAt(x+1));
+            }
+            else {
+                System.out.print(cumle.charAt(x));
+            }
+        }
 
     }
 }
